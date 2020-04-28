e4d9f1a582e88d5228df2ccb231c0b24e6de70ec
Michael Small <michael.small@uwa.edu.au>
2020-04-28 14:11:37 +0800

    predicting control phase

--- EpiSim.jl
+++ EpiSim.jl
@@ -13,6 +13,123 @@ module EpiSim
  #   ndays=120 #120 prediction (from patient(s) zere
  #   nsims=50 #50 runs
 nseeds=5 #
+
+function episim3(net1, net2, net3, epiparam, feth::Int64=100, predays::Int64=40, middays::Int64=30, postdays::Int64=80, nsims::Int64=50)
+    #feth is the threhold to switch between growth and control
+    pop=epiparam["pop"]
+    p0=epiparam["p0"]
+    p2=epiparam["p2"]
+    q=epiparam["q"]
+    r0=epiparam["r0"]
+    r2=epiparam["r2"]
+    nseeds=epiparam["nseeds"]
+
+    state=Array{Int8,2}(undef,1,pop) #this is a bit wasteful, there must be a better categorical way to do this...
+    state[1:pop].=1;
+    state[rand((1:pop),(1,nseeds))].=2; #seeds should be exposed cases
+
+    ndays=predays+postdays
+    st=Array{UInt64,2}(undef,ndays,nsims)
+    ex=Array{UInt64,2}(undef,ndays,nsims)
+    fe=Array{UInt64,2}(undef,ndays,nsims)
+    rm=Array{UInt64,2}(undef,ndays,nsims)
+    st[1,1:nsims] .= count(x->x==1,state)
+    ex[1,1:nsims] .= count(x->x==2,state)
+    fe[1,1:nsims] .= count(x->x==3,state)
+    rm[1,1:nsims] .= count(x->x==4,state)
+
+    iter = ProgressBar(1:nsims)
+    for j in iter
+        net=net1
+        #reinitialise the state vector
+        state=Array{Int8,2}(undef,1,pop) #this is a bit wasteful, there must be a better categorical way to do this...
+        state[1:pop].=1;
+        state[rand((1:pop),(1,nseeds))].=2; #seeds should be exposed cases
+        r=r0
+        p=p0
+
+        i=1
+        notdoneyet=true
+        alldone=false
+        donecounter=0
+        while i<=(predays+postdays)
+
+            for v in vertices(net)
+                if state[v]==1
+                    for n in all_neighbors(net, v)
+                        if state[n]==3 && rand(Float64).<p
+                            state[v]=2   #susceptible becomes exposed
+                        elseif state[n]==14 && rand(Float64).<p
+                            state[v]=2   #susceptible becomes exposed
+                        end
+                    end
+                elseif state[v]==2 && rand(Float64).<q
+                    state[v]=13 #exposed becomes infectious
+                elseif state[v]==3 && rand(Float64).<r
+                    state[v]=14 #infectious becomes removed
+                end
+            end
+            state[state.>10]= state[state.>10] .- 10
+            st[i,j] = count(x->x==1,state)
+            ex[i,j] = count(x->x==2,state)
+            fe[i,j] = count(x->x==3,state)
+            rm[i,j] = count(x->x==4,state)
+
+            #########################################
+            #switch network after 300 infections
+            inftot = pop-(st[i,j]+ex[i,j])
+            if notdoneyet && inftot>feth
+                net=net2 #change network structure
+                r=r2     #change the removal rate
+                p=p2     #and the infection rate
+                #realign everything - this is clunky, should be a nicer way to achieve the same thing...
+                if i>predays
+                        shft=i-predays
+                        for k in 1:predays
+                            st[k,j]=st[k+shft,j]
+                            ex[k,j]=ex[k+shft,j]
+                            fe[k,j]=fe[k+shft,j]
+                            rm[k,j]=rm[k+shft,j]
+                        end
+                elseif i<predays
+                        shft=predays-i
+                        for k in (predays-shft):-1:1
+                            st[k+shft,j]=st[k,j]
+                            ex[k+shft,j]=ex[k,j]
+                            fe[k+shft,j]=fe[k,j]
+                            rm[k+shft,j]=rm[k,j]
+                        end
+                        for k in 1:shft
+                            st[k,j]=pop
+                            ex[k,j]=0
+                            fe[k,j]=0
+                            rm[k,j]=0
+                        end
+                end
+                i=predays
+                notdoneyet=false
+            end
+            #########################################
+
+            #########################################
+            #switch again after middays infections
+            if ~notdoneyet && ~alldone
+                donecounter += 1
+                if donecounter>middays
+                    net=net3
+                    alldone=true
+                end
+            end
+
+            i += 1
+        end
+    end
+
+    return st, ex, fe, rm
+
+end
+
+
     function episim(net1, net2, epiparam, feth::Int64=100, predays::Int64=40, postdays::Int64=80, nsims::Int64=50)
         #feth is the threhold to switch between growth and control
         pop=epiparam["pop"]
