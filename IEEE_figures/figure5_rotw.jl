75895247dc2e183741bb860f29df53bfb537ba19
Michael Small <michael.small@uwa.edu.au>
2020-06-06 13:09:59 +0800

    reorganised

---
+++ IEEE_figures/figure5_rotw.jl
@@ -0,0 +1,87 @@
+cd("/Users/michael/work/GitHub/epinets")
+include("EpiSim.jl")
+
+using CSV
+using Plots
+using LightGraphs
+using JLD2, FileIO
+
+cd("/Users/michael/work/GitHub/COVID-19/csse_covid_19_data/csse_covid_19_time_series")
+
+file="time_series_covid19_confirmed_global.csv"
+allstates=["Hong Kong" "" "" "" "Shanghai" "Beijing"]
+countries=["China" "Singapore" "Finland" "Denmark" "China" "China"]
+z=Array{Any,1}(undef,length(allstates))
+for (i,state) in enumerate(allstates)
+    z[i]=EpiSim.getdata(countries[i],state)
+end
+
+ddays=CSV.File(file)[1]
+ddays=propertynames(ddays,4)[5:end]
+ddays=String.(ddays)
+ndays=length(ddays)
+
+pops=[7451000 5639000 5518000 5806000 24280000 21540000]
+cd("/Users/michael/work/GitHub/epinets")
+
+#'reasonable' parameters
+epiparam=Dict()
+epiparam["p0"]=0.2 #a guess - tuned to match observed data
+epiparam["p2"]=1/12 #revised infection rate with distancing measure
+epiparam["q"]=1/7 #"up to" two weeks
+epiparam["r0"]=1/14 #about two weeks for mild, 3-6 for severe
+epiparam["r2"]=1/4 #revised removal rate (now due to testing and isolation)
+epiparam["nseeds"]=5 #probably too many, consider dropping.
+
+allrs=Dict()
+totItps=Dict()
+tpdays=Dict()
+#tpdayman=[68 83 58 77 12 4 0 0]
+for (i,statename) in enumerate(allstates)
+#    if tpdayman[i]==0
+#        continue
+#    end
+    country=countries[i]
+    println("Working on ",statename,", ",country)
+    #get relevant data
+    gridsize=Int(floor(sqrt(pops[i])))
+    pop=gridsize^2
+    epiparam["gridsize"]=gridsize
+    epiparam["pop"]=pop
+    y=z[i];
+    #compute turning point
+    ddt(z,zt)=count(z->z>0, z[1:zt])+ count(z->z<0, z[zt+1:end])
+    ~,tpday=findmax([ddt(diff(diff(y)),nx) for nx in 1:(ndays-2)])
+    #this is the turning point between exponential growth and decay. totItp total infections at day tpday
+    totItp=y[tpday+1]
+    #build networks
+    bamodel=barabasi_albert(gridsize^2, 3, 2)
+    lattice=LightGraphs.grid((gridsize,gridsize),periodic=true)
+    wattstrog95=watts_strogatz(gridsize^2, 4, 0.013)  #s=0.013 => 95% compliance
+    wattstrog90=watts_strogatz(gridsize^2, 4, 0.026)  #s=0.026 => 90% compliance
+    wattstrog80=watts_strogatz(gridsize^2, 4, 0.053)  #s=0.053 => 80% compliance
+    #simulations
+    St95,Et95,It95,Rt95=EpiSim.episim(bamodel,wattstrog95, epiparam, totItp, tpday+2, 90, 100)
+    St90,Et90,It90,Rt90=EpiSim.episim(bamodel,wattstrog90, epiparam, totItp, tpday+2, 90, 100)
+    St80,Et80,It80,Rt80=EpiSim.episim(bamodel,wattstrog80, epiparam, totItp, tpday+2, 90, 100)
+    #store it all away
+    rs=Array{Any,2}(undef,3,4)
+    rs[1,1]=St95
+    rs[1,2]=Et95
+    rs[1,3]=It95
+    rs[1,4]=Rt95
+    rs[2,1]=St90
+    rs[2,2]=Et90
+    rs[2,3]=It90
+    rs[2,4]=Rt90
+    rs[3,1]=St80
+    rs[3,2]=Et80
+    rs[3,3]=It80
+    rs[3,4]=Rt80
+    allrs[statename]=deepcopy(rs)
+    totItps[statename]=totItp
+    tpdays[statename]=tpday
+    filetitle="done_"*join(split(statename))*"-"*join(split(country))
+    @save filetitle allrs rs z totItps tpdays allstates countries epiparam i
+end
+@save "done_ROTW" allrs z totItps tpdays allstates countries  epiparam
---
+++ IEEE_figures/param_sweep.jl
@@ -0,0 +1,185 @@
+#as before, because, why not....
+using LightGraphs
+using GraphPlot
+using Plots
+using ProgressBars
+using Statistics
+using LinearAlgebra
+using JLD2
+
+#epidemic simulation function
+function episim(net1, net2, ndays::Int64=120, nsims::Int64=50, p2::Float64=1/12, r2::Float64=1/4)
+
+    state=Array{Int8,2}(undef,1,pop) #this is a bit wasteful, there must be a better categorical way to do this...
+    state[1:pop].=1;
+    state[rand((1:pop),(1,nseeds))].=2; #seeds should be exposed cases
+
+    st=Array{UInt64,2}(undef,ndays,nsims)
+    ex=Array{UInt64,2}(undef,ndays,nsims)
+    fe=Array{UInt64,2}(undef,ndays,nsims)
+    rm=Array{UInt64,2}(undef,ndays,nsims)
+    st[1,1:nsims] .= count(x->x==1,state)
+    ex[1,1:nsims] .= count(x->x==2,state)
+    fe[1,1:nsims] .= count(x->x==3,state)
+    rm[1,1:nsims] .= count(x->x==4,state)
+
+    print("p2=$p2; r2=$r2")
+
+    iter = ProgressBar(1:nsims)
+    for j in iter
+        net=net1
+        #reinitialise the state vector
+        state=Array{Int8,2}(undef,1,pop) #this is a bit wasteful, there must be a better categorical way to do this...
+        state[1:pop].=1;
+        state[rand((1:pop),(1,nseeds))].=2; #seeds should be exposed cases
+        r=r0
+        p=p0
+
+        for i in 1:ndays
+
+            for v in vertices(net)
+                if state[v]==1
+                    for n in all_neighbors(net, v)
+            +
+                        if state[n]==3 && rand(Float64).<p
+                            state[v]=2   #susceptible becomes exposed
+                        elseif state[n]==14 && rand(Float64).<p
+                            state[v]=2   #susceptible becomes exposed
+                        end
+                    end
+                elseif state[v]==2 && rand(Float64).<q
+                    state[v]=13 #exposed becomes infectious
+                elseif state[v]==3 && rand(Float64).<r
+                    state[v]=14 #infectious becomes removed
+                end
+            end
+            state[state.>10] = state[state.>10] .- 10
+
+            st[i,j] = count(x->x==1,state)
+            ex[i,j] = count(x->x==2,state)
+            fe[i,j] = count(x->x==3,state)
+            rm[i,j] = count(x->x==4,state)
+
+            #########################################
+            #switch network after 300 infections
+            if fe[i,j]>100
+                net=net2 #change network structure
+                r=r2     #change the removal rate
+                p=p2     #and the infection rate
+            end
+            #########################################
+
+        end
+    end
+
+    return st, ex, fe, rm
+
+end
+
+
+#requires global parameters
+gridsize=1450
+pop=gridsize^2
+p0=0.2 #a guess - tuned to match observed data
+p2=1/12 #revised infection rate with distancing measures
+q=1/2 #"up to" two weeks
+r0=1/14 #about two weeks for mild, 3-6 for severe
+r2=1/4 #revised removal rate (now due to testing and isolation)
+ #   ndays=120 #120 prediction (from patient(s) zere
+ #   nsims=50 #50 runs
+nseeds=5 #probably too many, consider dropping.
+
+#and networks:
+swp=0.01
+bamodel=barabasi_albert(gridsize^2, 3, 2)
+#lattice=LightGraphs.grid((gridsize,gridsize),periodic=true)
+wattstrog=watts_strogatz(gridsize^2, 4, swp)
+
+
+#pretty plot - I can't believe this is not built in
+function plotquantiles(y,col,labl,qnt::Float64=0.3)
+    nt,ny=size(y)
+    low=Array{Float64,1}(undef,nt)
+    mid=Array{Float64,1}(undef,nt)
+    hig=Array{Float64,1}(undef,nt)
+    for i in 1:nt
+        low[i],mid[i],hig[i] = quantile(y[i,:],[0.5-qnt, 0.5, 0.5+qnt])
+    end
+    plot!(1:nt,mid,grid=false,ribbon=(mid-low,hig-mid),fillalpha=.25,lw=3, seriescolor=col, label=labl)
+end
+
+
+#performance evaluation metrics
+function recover50(A) #how many days does it take to eliminate infection in 50% of simulations?
+    ndays,nsims=size(A)
+    epigone=sum(x->x<1, A, dims=2) #number of zeros per column
+    days=findall(x->x>(nsims/2),vec(epigone)) #days when epidemic is zero
+    days=sort(days)
+    if isempty(days)
+        return -1   #never recovers
+    end
+    dayone=1
+    while dayone==days[1] && length(days)>1
+        dayone += 1
+        deleteat!(days, 1)
+    end
+    if length(days)==1
+        return 0 #never endemic
+    else
+        return days[1]
+    end
+end
+#performance evaluation metrics
+
+function meantotalinfected(A) #total infected
+    ndays,nsims=size(A)
+    return mean(A[end,:])
+end
+
+function meanmaxinfected(A) #maximumload
+    return mean(maximum(A, dims=2)) #not sure about dims here
+end
+
+function parametersweep(swaps,p2s,r2s)
+    ndays=300#0
+    nsims=20#100
+
+    ns=length(swaps)
+    np=length(p2s)
+    nr=length(r2s)
+    mti=Array{Float64,3}(undef,ns,np,nr)
+    r50=Array{Int64,3}(undef,ns,np,nr)
+    mmi=Array{Float64,3}(undef,ns,np,nr)
+
+    si=1
+    bigiter=ProgressBar(swaps)
+    for swp in bigiter
+        wattstrog=watts_strogatz(gridsize^2, 4, swp)
+        ri = 1
+        for r2 in r2s
+            pi=1
+            for p2 in p2s
+                St,Et,It,Rt = episim(bamodel,wattstrog,ndays,nsims,p2,r2) #switch between scale free and lattice (i.e. mixing to diffusion)
+                r50[si,pi,ri] = recover50(Et+It) #irradication in 50% of simulations
+                mti[si,pi,ri] = meantotalinfected(Rt) #total infections
+                mmi[si,pi,ri] = meanmaxinfected(Rt) #highest total caseload
+                pi += 1
+            end
+            ri += 1
+        end
+        print("Done swp=$(swp)")
+        si += 1
+    end
+
+    return mti, mmi, r50
+end
+
+mdeg=4;
+C=[1, 0.99, 0.95, 0.9, 0.8, 0.7, 0.6, 0.5]
+swaps= 1 .- C.^(1 ./ mdeg)
+p2s=[0.02:0.02:0.2;]
+r2s=[0.2:0.05:0.5;]
+
+mti,mmi,r50 = parametersweep(swaps,p2s,r2s)
+
+@save("parameters_swept2020_th100")
